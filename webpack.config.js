c4226abdd89ad39eb09670d25aba1b8384feec8f
Riad <riad-esrefov@mail.ru>
2023-04-28 00:14:32 +0300

    CopyWebpackPlugin added to config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 
 module.exports = {
@@ -7,18 +8,30 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./index.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "assets", to: "" },
+        // { from: "other", to: "public" },
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader","css-loader"],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg/,
+        type: "asset/inline",
       },
     ],
   },
-  devServer:{
-    port: 3000
-  }
+  devServer: {
+    port: 3000,
+  },
 };
